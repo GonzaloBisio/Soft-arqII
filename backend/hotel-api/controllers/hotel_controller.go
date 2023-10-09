b864b63e7f575815226f77d5fe763231cc52af80
Agustin Glaiel <agustinglaiel@gmail.com>
2023-10-09 20:36:40 -0300

    agregue lo de rabbit, habria que probarlo

--- backend/hotel-api/controllers/hotel_controller.go
+++ backend/hotel-api/controllers/hotel_controller.go
@@ -3,12 +3,12 @@ package controllers
 import (
 	"hotel-api/models"
 	"hotel-api/services"
+	"hotel-api/utils/queue"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-// CreateHotel crea un nuevo hotel.
 func CreateHotel(c *gin.Context) {
     var newHotel models.Hotel
     if err := c.ShouldBindJSON(&newHotel); err != nil {
@@ -22,10 +22,34 @@ func CreateHotel(c *gin.Context) {
         return
     }
 
+    // Configura la conexión a RabbitMQ
+    rabbitMQConfig := queue.RabbitMQConfig{
+        Username: "tu_usuario",
+        Password: "tu_contraseña",
+        Host:     "localhost",
+        Port:     "5672",
+    }
+    rabbitMQ, err := queue.NewRabbitMQQueue(rabbitMQConfig)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al configurar RabbitMQ"})
+        return
+    }
+    defer rabbitMQ.Close()
+
+    //Este es el mensaje que se envia a la cola
+    message := "Se creó un nuevo hotel: " + createdHotel.Name
+    // Nombre de la cola
+    queueName := "hotel_creation"
+    // Envia el mensaje a la cola
+    err = rabbitMQ.PublishMessage(queueName, message)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al enviar mensaje a la cola RabbitMQ"})
+        return
+    }
+
     c.JSON(http.StatusCreated, createdHotel)
 }
 
-// GetHotelByID obtiene un hotel por ID.
 func GetHotelByID(c *gin.Context) {
     hotelID := c.Param("id")
 
@@ -38,7 +62,6 @@ func GetHotelByID(c *gin.Context) {
     c.JSON(http.StatusOK, hotel)
 }
 
-// UpdateHotel actualiza un hotel.
 func UpdateHotel(c *gin.Context) {
     var updatedHotel models.Hotel
     if err := c.ShouldBindJSON(&updatedHotel); err != nil {
@@ -49,6 +72,28 @@ func UpdateHotel(c *gin.Context) {
     updatedHotel, err := services.HotelService.UpdateHotel(updatedHotel)
     if err != nil {
         c.JSON(err.Status(), err)
+        return
+    }
+
+    rabbitMQConfig := queue.RabbitMQConfig{
+        Username: "tu_usuario",
+        Password: "tu_contraseña",
+        Host:     "localhost",
+        Port:     "5672",
+    }
+    rabbitMQ, err := queue.NewRabbitMQQueue(rabbitMQConfig)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al configurar RabbitMQ"})
+        return
+    }
+    defer rabbitMQ.Close()
+
+    message := "Se actualizó un hotel: " + updatedHotel.Name
+    queueName := "hotel_update"
+    
+    err = rabbitMQ.PublishMessage(queueName, message)
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al enviar mensaje a la cola RabbitMQ"})
         return
     }
 
---
+++ backend/hotel-api/utils/queue/queue.go
@@ -0,0 +1,83 @@
+package queue
+
+import (
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+//la configuracion de rabbit
+type RabbitMQConfig struct {
+    Username string
+    Password string
+    Host     string
+    Port     string
+}
+
+// esto representa lo que seria una ocla de RabbitMQ
+type RabbitMQQueue struct {
+    conn    *amqp.Connection
+    channel *amqp.Channel
+    config  RabbitMQConfig
+}
+
+// instancia de RabbitMQQueue.
+func NewRabbitMQQueue(config RabbitMQConfig) (*RabbitMQQueue, error) {
+    conn, err := amqp.Dial("amqp://" + config.Username + ":" + config.Password + "@" + config.Host + ":" + config.Port + "/")
+    if err != nil {
+        return nil, err
+    }
+
+    channel, err := conn.Channel()
+    if err != nil {
+        return nil, err
+    }
+
+    return &RabbitMQQueue{
+        conn:    conn,
+        channel: channel,
+        config:  config,
+    }, nil
+}
+
+func (q *RabbitMQQueue) PublishMessage(queueName, message string) error {
+    _, err := q.channel.QueueDeclare(
+        queueName, // Nombre de la cola
+        false,     // Durable
+        false,     // AutoDelete
+        false,     // Exclusive
+        false,     // NoWait
+        nil,       // Args
+    )
+    if err != nil {
+        return err
+    }
+
+    // Publica el mensaje en la cola
+    err = q.channel.Publish(
+        "",         // Exchange
+        queueName,  // Queue
+        false,      // Mandatory
+        false,      // Immediate
+        amqp.Publishing{
+            ContentType: "text/plain",
+            Body:        []byte(message),
+        },
+    )
+    if err != nil {
+        return err
+    }
+
+    log.Printf("Mensaje enviado a la cola %s: %s", queueName, message)
+    return nil
+}
+
+// Close cierra la conexión y el canal de RabbitMQ.
+func (q *RabbitMQQueue) Close() {
+    if q.channel != nil {
+        q.channel.Close()
+    }
+    if q.conn != nil {
+        q.conn.Close()
+    }
+}
