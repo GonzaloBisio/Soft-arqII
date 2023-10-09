5252cb74889f5c8562215881b1edc6e380612c65
Agustin Glaiel <agustinglaiel@gmail.com>
2023-10-09 16:34:50 -0300

    controllers

--- src/hotel-api/controllers/hotel_controller.go
+++ src/hotel-api/controllers/hotel_controller.go
@@ -1,19 +1,64 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"hotel-api/models"
+	"hotel-api/services"
+
+	"github.com/gorilla/mux"
 )
 
 func CreateHotel(w http.ResponseWriter, r *http.Request) {
-    // Lógica para crear un hotel en MongoDB y notificar a RabbitMQ
+    // Aquí puedes implementar la lógica para crear un nuevo hotel.
+    // Puedes usar el cuerpo de la solicitud (r.Body) para obtener los datos del hotel
+    // y luego utilizar el servicio HotelService para insertar el hotel en la base de datos.
+    // Por ejemplo:
+
+    var newHotel models.Hotel
+    err := json.NewDecoder(r.Body).Decode(&newHotel)
+    if err != nil {
+        http.Error(w, "Error al decodificar la solicitud", http.StatusBadRequest)
+        return
+    }
+
+    // Llama a la función InsertHotel del servicio HotelService
+    createdHotel, apiErr := services.HotelService.InsertHotel(newHotel)
+    if apiErr != nil {
+        http.Error(w, apiErr.Message, apiErr.Status)
+        return
+    }
+
+    // Devuelve el hotel creado en la respuesta
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(createdHotel)
 }
 
 func GetHotelByID(w http.ResponseWriter, r *http.Request) {
-    // Lógica para obtener un hotel por ID
+    // Aquí puedes implementar la lógica para obtener un hotel por su ID.
+    // Puedes utilizar el paquete "github.com/gorilla/mux" para obtener el ID desde
+    // la ruta de la solicitud (por ejemplo, r.URL.Path).
+
+    vars := mux.Vars(r)
+    hotelID := vars["id"]
+
+    // Llama a la función GetHotelById del servicio HotelService
+    hotel, apiErr := services.HotelService.GetHotelById(hotelID)
+    if apiErr != nil {
+        http.Error(w, apiErr.Message, apiErr.Status)
+        return
+    }
+
+    // Devuelve el hotel en la respuesta
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(hotel)
 }
 
 func UpdateHotel(w http.ResponseWriter, r *http.Request) {
-    // Lógica para modificar un hotel en MongoDB y notificar a RabbitMQ
+    // Aquí puedes implementar la lógica para actualizar un hotel en la base de datos.
+    // Similar a la función CreateHotel, debes obtener los datos del hotel del cuerpo
+    // de la solicitud y luego utilizar el servicio HotelService para realizar la actualización.
 }
 
 // ... otros controladores
