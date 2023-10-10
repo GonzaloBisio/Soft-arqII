ec821821ae28efab89e9bd201ffa04ffded0d30b
Agustin Glaiel <agustinglaiel@gmail.com>
2023-10-10 16:34:01 -0300

    main

--- backend/hotel-api/main.go
+++ backend/hotel-api/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"hotel-api/dao"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/streadway/amqp"
 )
 
@@ -11,18 +13,35 @@ var rabbitMQConn *amqp.Connection
 var rabbitMQCh *amqp.Channel
 
 func init() {
-	dao.InitializeMongoClient()
-	rabbitMQURL := "amqp://tu_usuario:tu_contraseña@localhost:5672/"
-	var err error
-	rabbitMQConn, err = amqp.Dial(rabbitMQURL)
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Inicializa el cliente MongoDB
+    dao.InitializeMongoClient()
 
-	// Abre un canal de RabbitMQ
-	rabbitMQCh, err = rabbitMQConn.Channel()
-	if err != nil {
-		log.Fatal(err)
-	}
+    // Configura la conexión a RabbitMQ
+    rabbitMQURL := "amqp://tu_usuario:tu_contraseña@localhost:5672/"
+    var err error
+    rabbitMQConn, err = amqp.Dial(rabbitMQURL)
+    if err != nil {
+        log.Fatal(err)
+    }
 
+    // Abre un canal de RabbitMQ
+    rabbitMQCh, err = rabbitMQConn.Channel()
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Declara las colas de RabbitMQ según tus necesidades
+    queueName := "hotel_creation"
+    _, err = rabbitMQCh.QueueDeclare(queueName, false, false, false, false, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func main() {
+    r := mux.NewRouter()
+
+    if err := http.ListenAndServe(":3000", r); err != nil {
+        log.Fatal(err)
+    }
 }
