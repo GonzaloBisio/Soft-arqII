6098be14fdb19eecf554e434e7b14a0c8d519e88
Gonzalo <gbisio@itlatingroup.cl>
2023-10-12 14:08:45 -0300

    a

--- backend/hotel-api/models/hotel.go
+++ backend/hotel-api/models/hotel.go
@@ -2,14 +2,12 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-
-
 type Hotel struct {
 	ID          primitive.ObjectID `bson:"_id" json:"id"`
-	Name        string        `bson:"name" json:"name"`
-	Description string        `bson:"description" json:"description"`
+	Name        string             `bson:"name" json:"name"`
+	Description string             `bson:"description" json:"description"`
 }
 
-type Hotels struct{
+type Hotels struct {
 	Hotels []Hotel `json:"hotels"`
 }
