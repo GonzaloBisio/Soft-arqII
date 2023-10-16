b0b4b91125a98537848c96c74a3e3da9aa15c86a
Gonzalo <gbisio@itlatingroup.cl>
2023-10-16 20:21:42 -0300

    update hotel model

--- backend/hotel-api/models/hotel.go
+++ backend/hotel-api/models/hotel.go
@@ -2,10 +2,18 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+type Reserva struct {
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	FechaIni string             `bson:"fechaIni" json:"fechaIni"`
+	FechaFin string             `bson:"fechaFin" json:"fechaFin"`
+}
 type Hotel struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name        string             `bson:"name" json:"name"`
 	Description string             `bson:"description" json:"description"`
+	Fotos       []string           `bson:"fotos" json:"fotos"`
+	Amenities   []string           `bson:"amenities" json:"amenities"`
+	Reservas    []Reserva          `bson:"reservas" json:"reservas"`
 }
 
 type Hotels struct {
