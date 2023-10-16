816fe5f67beb079b8a943a18b22b8dd11280c94c
Julian Ahumada <julianahumada86@gmail.com>
2023-10-16 19:45:58 -0300

    rabbit fuera del main

--- backend/hotel-api/main.go
+++ backend/hotel-api/main.go
@@ -2,47 +2,7 @@ package main
 
 import (
 	app "hotel-api/app"
-	"hotel-api/dao"
-	"log"
-
-	"github.com/streadway/amqp"
 )
-
-var rabbitMQCh *amqp.Channel
-var rabbitMQConn *amqp.Connection
-
-func init() {
-
-	// Inicializa el cliente MongoDB
-	dao.InitializeMongoClient()
-
-	// Configura la conexión a RabbitMQ
-	rabbitMQURL := "amqp://user:password@localhost:5672/"
-	var err error
-	rabbitMQConn, err = amqp.Dial(rabbitMQURL)
-	if err != nil {
-		log.Fatal(err)
-
-	} else {
-		log.Println("Rabit cargado")
-	}
-
-	// Abre un canal de RabbitMQ
-	rabbitMQCh, err = rabbitMQConn.Channel()
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		log.Println("Rabit Canal creado")
-	}
-
-	// Declara las colas de RabbitMQ según tus necesidades
-	queueName := "hotel_creation"
-	_, err = rabbitMQCh.QueueDeclare(queueName, false, false, false, false, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
 
 func main() {
 
