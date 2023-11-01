f5acdbcf882ead1e23d2385424b05de99c7e1d96
Agustin Glaiel <agustinglaiel@gmail.com>
2023-11-01 17:58:57 -0300

    dtos y models

--- backend/user-reserva-disponibilidad-api/dtos/hotel_dto.go
+++ backend/user-reserva-disponibilidad-api/dtos/hotel_dto.go
@@ -1,1 +1,12 @@
 package dtos
+
+type hotelDto struct {
+	Id               int    `json:"id"`
+	HotelName        string `json:"hotel_name"`
+	IdMongo          string `json:"id_mongo"`
+	IdAmadeus        string `json:"id_amadeus"`
+}
+
+type hotelsDto []hotelDto
+
+
--- backend/user-reserva-disponibilidad-api/dtos/user_dto.go
+++ backend/user-reserva-disponibilidad-api/dtos/user_dto.go
@@ -1,1 +1,14 @@
 package dtos
+
+type userDto struct {
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	LastName string `json:"last_name"`
+	UserName string `json:"username"`
+	Address  string `json:"address"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+	Type     bool   `json:"type"`
+}
+
+type usersDto []userDto
